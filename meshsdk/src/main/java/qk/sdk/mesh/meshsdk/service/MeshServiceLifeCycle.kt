29b2b62bd4e66d2d8ae742c69d69c795cc4e6535
陈思创 <chensc@mxchip.com>
2020-09-18 13:14:49 +0800

    1.MeshServiceLifeCycle补充提交

---
+++ meshsdk/src/main/java/qk/sdk/mesh/meshsdk/service/MeshServiceLifeCycle.kt
@@ -0,0 +1,11 @@
+package qk.sdk.mesh.meshsdk.service
+
+/**
+ * @des:
+ * @author: chensc@mxchip.com
+ * @date: 2020/9/17 3:57 PM
+ */
+open interface MeshServiceLifeCycle {
+    fun onCreate()
+    fun onDestory()
+}
